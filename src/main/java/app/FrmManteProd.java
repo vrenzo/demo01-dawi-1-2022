b30e050ac678248b8df84badcdfa93ed612df044
vrenzo <i202014286@cibertec.edu.pe>
2022-04-22 22:21:30 -0500

    proyecto con listado, registro y busqueda

--- src/main/java/app/FrmManteProd.java
+++ src/main/java/app/FrmManteProd.java
@@ -143,6 +143,15 @@ public class FrmManteProd extends JFrame {
 		cboProvedor.setBounds(315, 103, 86, 22);
 		contentPane.add(cboProvedor);
 		
+		JButton btnBuscar = new JButton("Buscar");
+		btnBuscar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				buscarProducto();
+			}
+		});
+		btnBuscar.setBounds(324, 62, 89, 23);
+		contentPane.add(btnBuscar);
+		
 		llenaCombo();
 	}
 
@@ -191,6 +200,20 @@ public class FrmManteProd extends JFrame {
 			txtSalida.append("Proveedor..:" + p.getProveedor().getNombre_rs() + "\n" );
 			txtSalida.append("*******************************************\n" );
 		}
+	}
+	
+	void buscarProducto() {
+			EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("mysql");
+			EntityManager em = fabrica.createEntityManager();
+					
+			Producto p = em.find(Producto.class, txtCódigo.getText());
+					
+			if (p != null) {
+				txtDescripcion.setText(p.getDescripcion());
+			} else
+				txtSalida.setText("Codigo No Existe Mongol!!");
+				
+				em.close();	
 	}
 	
 	void registrar() {
